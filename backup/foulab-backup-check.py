562a636003f6100ecfbc0d758bb164e21445641b
eigma <eigma@foulab.org>
2025-09-08 22:19:38 -0400

    yapf format

--- backup/foulab-backup-check.py
+++ backup/foulab-backup-check.py
@@ -9,6 +9,7 @@ import re
 import subprocess, mattermostdriver, glob
 
 import gettext
+
 gettext.install("duplicity", names=["ngettext"])
 import duplicity.backend
 import duplicity.backends.localbackend
@@ -18,22 +19,24 @@ import duplicity.log
 import duplicity.path
 
 HOSTS = [
-  'bahamut.lab',
-  'foubot.lab',
-  'foudoor.lab',
-  'foulab.org',
-  'manning.lab',
-  'test.foulab.org',
+    'bahamut.lab',
+    'foubot.lab',
+    'foudoor.lab',
+    'foulab.org',
+    'manning.lab',
+    'test.foulab.org',
 ]
 assert len(HOSTS) == len(set(HOSTS))
 assert HOSTS == sorted(HOSTS)
 
 mail = ''
 
+
 def print_tee(text):
   global mail
   print(text)
   mail += text + '\n'
+
 
 def check(dir_path):
   got = set(os.listdir(dir_path))
@@ -43,29 +46,37 @@ def check(dir_path):
 
   for host in HOSTS:
     text = f'- {host}: '
-    parsed_url = duplicity.backend.ParsedUrl('file://' + os.path.join(dir_path, host))
+    parsed_url = duplicity.backend.ParsedUrl('file://' +
+                                             os.path.join(dir_path, host))
     local_backend = duplicity.backends.localbackend.LocalBackend(parsed_url)
     backend = duplicity.backend.BackendWrapper(local_backend)
-    col_stats = duplicity.dup_collections.CollectionsStatus(backend, duplicity.config.archive_dir_path).set_values()
-    
+    col_stats = duplicity.dup_collections.CollectionsStatus(
+        backend, duplicity.config.archive_dir_path).set_values()
+
     chain = col_stats.get_last_backup_chain()
     if not chain:
       text += '❗ No chain'
     else:
+
       def _set_str(s):
         t = datetime.datetime.fromtimestamp(s.get_time(), tz=datetime.UTC)
         info = backend.query_info(s.volume_name_dict.values())
         size = sum(i['size'] for i in info.values())
         # TODO: size sanity check
         return f'{t.strftime("%b %-d")} ({(now - t).days} days ago, {size / 1024 / 1024 / 1024:.01f} GB)'
+
       fullset = chain.get_first()
       text += f'Full: {_set_str(fullset)}'
-      if datetime.datetime.fromtimestamp(fullset.get_time(), tz=datetime.UTC) < now - datetime.timedelta(days=37):
+      if datetime.datetime.fromtimestamp(
+          fullset.get_time(),
+          tz=datetime.UTC) < now - datetime.timedelta(days=37):
         text += f' ❗ Older than 37 days'
       lastset = chain.get_last()
       if lastset is not fullset:
         text += f', Incremental: {_set_str(lastset)}'
-        if datetime.datetime.fromtimestamp(lastset.get_time(), tz=datetime.UTC) < now - datetime.timedelta(days=3):
+        if datetime.datetime.fromtimestamp(
+            lastset.get_time(),
+            tz=datetime.UTC) < now - datetime.timedelta(days=3):
           text += f' ❗ Older than 3 days'
     print_tee(text)
 
@@ -73,13 +84,16 @@ def check(dir_path):
   for host in sorted(extra):
     print_tee(f'Extra host: {host}')
 
+
 def check_wifi(dir_path):
   now = datetime.datetime.now(datetime.UTC)
   host = 'wifi.lab'
   text = '- wifi.lab: '
 
   def _set_str(label, regexp, scale):
-    files = [f for f in os.listdir(f'{dir_path}/wifi.lab/') if re.match(regexp, f)]
+    files = [
+        f for f in os.listdir(f'{dir_path}/wifi.lab/') if re.match(regexp, f)
+    ]
 
     text = f'{label}: '
     if not files:
@@ -98,8 +112,10 @@ def check_wifi(dir_path):
 
   text += _set_str('MTD Full', re.compile(r'mtd-full.(.*).img.p7m'), 'MB')
   text += ', '
-  text += _set_str('Sysupgrade Full', re.compile(r'sysupgrade-full.(.*).tar.gz.p7m'), 'KB')
+  text += _set_str('Sysupgrade Full',
+                   re.compile(r'sysupgrade-full.(.*).tar.gz.p7m'), 'KB')
   print_tee(text)
+
 
 def check_cinderblock(dir_path):
   now = datetime.datetime.now(datetime.UTC)
@@ -107,7 +123,10 @@ def check_cinderblock(dir_path):
   text = '- cinderblock.lab: '
 
   def _set_str(label, regexp, scale, max_age):
-    files = [f for f in os.listdir(f'{dir_path}/cinderblock.lab/') if re.match(regexp, f)]
+    files = [
+        f for f in os.listdir(f'{dir_path}/cinderblock.lab/')
+        if re.match(regexp, f)
+    ]
 
     text = f'{label}: '
     if not files:
@@ -124,17 +143,27 @@ def check_cinderblock(dir_path):
         text += f' ❗ Older than 7 days'
     return text
 
-  text += _set_str('ZFS Full', re.compile(r'zroot-full.(.*).zfs.p7m'), 'GB', max_age=datetime.timedelta(days=37))
+  text += _set_str('ZFS Full',
+                   re.compile(r'zroot-full.(.*).zfs.p7m'),
+                   'GB',
+                   max_age=datetime.timedelta(days=37))
   text += ', '
-  text += _set_str('ZFS Incremental', re.compile(r'zroot-inc.(.*).zfs.p7m'), 'GB', max_age=datetime.timedelta(days=7))
+  text += _set_str('ZFS Incremental',
+                   re.compile(r'zroot-inc.(.*).zfs.p7m'),
+                   'GB',
+                   max_age=datetime.timedelta(days=7))
   text += ', '
-  text += _set_str('Conf Full', re.compile(r'conf-full.(.*).tar.p7m'), 'MB', max_age=datetime.timedelta(days=7))
+  text += _set_str('Conf Full',
+                   re.compile(r'conf-full.(.*).tar.p7m'),
+                   'MB',
+                   max_age=datetime.timedelta(days=7))
   print_tee(text)
+
 
 def main():
   parser = argparse.ArgumentParser(
-                    prog='foulab-backup-check',
-                    description='Checks backups for consistency and completness')
+      prog='foulab-backup-check',
+      description='Checks backups for consistency and completness')
   parser.add_argument('-d', required=True, help='Root directory of backups')
   parser.add_argument('-e', action='store_true', help='Send e-mail')
   parser.add_argument('-m', action='store_true', help='Post to Mattermost')
@@ -161,22 +190,31 @@ def main():
     m['Subject'] = 'Foulab Backup Check'
     m.set_content(mail)
 
-    subprocess.run(['/sbin/sendmail', '-t', '-oi'], input=m.as_string(), encoding='ascii', check=True)
+    subprocess.run(['/sbin/sendmail', '-t', '-oi'],
+                   input=m.as_string(),
+                   encoding='ascii',
+                   check=True)
 
   if args.m:
     mattermost = mattermostdriver.Driver({
-      'url': 'test.foulab.org',
-      'port': 443,
-      'login_id': 'bahamut',
-      'token': open('/usr/local/etc/foulab-deaddrop-mattermost-token').read().strip(),
+        'url':
+            'test.foulab.org',
+        'port':
+            443,
+        'login_id':
+            'bahamut',
+        'token':
+            open('/usr/local/etc/foulab-deaddrop-mattermost-token'
+                ).read().strip(),
     })
     mattermost.login()
 
     mattermost.posts.create_post({
-      # 'channel_id': 'na45qgqrsidcpjo6rorar4excy',  # DM to eigma
-      'channel_id': 'wnh3qnh4xp8h5qa3ic7gkaiy5c',  # Tech Support
-      'message': '**Backup Check**:\n\n' + mail,
+        # 'channel_id': 'na45qgqrsidcpjo6rorar4excy',  # DM to eigma
+        'channel_id': 'wnh3qnh4xp8h5qa3ic7gkaiy5c',  # Tech Support
+        'message': '**Backup Check**:\n\n' + mail,
     })
+
 
 if __name__ == '__main__':
   main()
